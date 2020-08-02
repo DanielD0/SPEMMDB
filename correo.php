d86ff9c96d4a3923731c43761cc8d4df6ceaffb6
CristianOrtega <desarrollo18@warriorslabs.com>
2020-08-02 17:43:29 -0500

    correo php

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])){
+    if(!empty($_POST['nombre'])&& !empty($_POST['email'])&& !empty($_POST['asunto'])&& !empty($_POST['mensaje'])){
+        $nombre = $_POST['nombre'];
+        $correo = $_POST['email'];
+        $asunto = $_POST['asunto'];
+        $mensaje = $_POST['mensaje'];
+        $header = "From: cristian.py.isc@gmailc.com" . "\r\n";
+        $header .= "Reply-To: cristian.py.isc.com" ."\r\n";
+        $header .= "X-Mailer: PHP/" . phpversion();
+        $submit = mail($nombre,$asunto,$asunto,$mensaje);
+    }
+}
+
+?>
